25ad9444cb4f0344a0699ceb4dc4b0578d4a635d
Long Pham <philong.bmt@gmail.com>
2025-05-05 17:06:03 +0700

    add selfui binary

--- Package.swift
+++ Package.swift
@@ -12,12 +12,19 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "self_ios_sdk",
-            targets: ["self_ios_sdk"]),
+            targets: ["self_ios_sdk", "SelfUI"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/krzyzanowskim/OpenSSL.git", .upToNextMinor(from: "1.1.1900"))
     ],
     targets: [
         .binaryTarget(
                     name: "self_ios_sdk",
                     path: "Frameworks/self_ios_sdk.xcframework"
+                ),
+        .binaryTarget(
+                    name: "SelfUI",
+                    path: "Frameworks/SelfUI.xcframework"
                 ),
         .testTarget(
             name: "self_ios_sdkTests",
